96c55518224edcadc8ca16940b067e800b15e23b
HumbertoAraujo <humbertovt@yahoo.com.br>
2022-06-02 19:09:26 -0300

    Add files script.js and picture click animation

---
+++ script.js
@@ -0,0 +1,14 @@
+function animationPicture(){
+    const picture = document.getElementById('foto');
+    picture.addEventListener('click', function(){
+        window.animatelo.flip('#foto');
+        window.animatelo.jello('.nav');
+    })
+
+}
+
+AOS.init();
+window.animatelo.flip('#foto');
+window.animatelo.fadeIn('#paragrafo');
+window.animatelo.jello('.nav');
+animationPicture();
