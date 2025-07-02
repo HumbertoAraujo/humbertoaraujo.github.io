9eb731979a593b15ffbbc47a12069b37ea580bba
Humberto Araujo <humberto.araujo@slbg.local>
2025-07-02 15:51:38 -0300

    Teste mobile 2

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-function animationPicture(){
-    const picture = document.querySelector('.profile-img');
-    picture.addEventListener('click', function(){
-           })
-}
-
-AOS.init();
-window.animatelo.flip('.profile-img');
-animationPicture();
-
-const toggle = document.querySelector('.mobile-toggle');
-const menu = document.querySelector('.nav-menu');
-toggle.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
+  // Menu Mobile
+  const toggle = document.querySelector('.mobile-toggle');
+  const menu = document.querySelector('.nav-menu');
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+  });
+  
